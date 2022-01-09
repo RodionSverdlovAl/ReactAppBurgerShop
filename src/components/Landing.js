3bea22c8ce1bf839b111b0241df90c2288aab340
RodionSverdlovAl <rsverdlovalexandrovich@gmail.com>
2022-01-09 13:31:06 +0300

    add styles and logic of component landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,30 +2,50 @@ import React from "react";
 import restaurants from '../sample-restaurants'
 
 class Landing extends React.Component {
+
+    state = {
+        display: false,
+        title: '',
+        url: '',
+    }
+
+    displayList = () =>{
+        const {display} = this.state;
+        this.setState({display: !display})
+    }
+    getTitle =(restaurants)=>{
+        const {title, url} = restaurants;
+        this.setState({title, url, display:false})
+    }
+    goToRestaurant = () =>{
+        console.log("Go to restaurant");
+    }
+
     render(){
         return(
             <>
                 <div className = "restaurant_select">
                     <div className = "restaurant_select_top">
-                        <div className = "restaurant_select_top-header">Выберите ресторан</div>
+                        <div onClick={this.displayList} className = "restaurant_select_top-header font-effect-outline">
+                            {this.state.title ? this.state.title :'Выберите ресторан'}
+                            </div>
                         <div className = "arrow_picker">
                             <div className = "arrow_picker-up"></div>
                             <div className = "arrow_picker-down"></div>
                         </div>
                     </div>
 
-                    <div className = "restaurant_select_bottom">
+                    {this.state.display?<div className = "restaurant_select_bottom">
                         <ul>
                             {
                                 restaurants.map(restaurants =>{
-                                    return <li key ={restaurants.id}>{restaurants.title}</li>
+                                    return <li onClick={()=>this.getTitle(restaurants)} key ={restaurants.id}>{restaurants.title}</li>
                                 })
                             }
                         </ul>
-                    </div>
+                    </div>:null}
+                    {this.state.title && !this.state.display ? <button onClick={this.goToRestaurant}>Перейти в ресторан</button>: null}
                 </div>
-
-                <button>Перейти в ресторан</button>
             </>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {render} from "react-dom"
 import Landing from './components/Landing'
-
+import './css/style.css';
 render(<Landing/>, document.querySelector('#root'))
 
