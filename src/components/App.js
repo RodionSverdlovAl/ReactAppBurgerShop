c7f894080b425fed39531a491266a67ecf8508ec
RodionSverdlovAl <rsverdlovalexandrovich@gmail.com>
2022-01-11 11:32:22 +0300

    add functional for Order component

--- src/components/App.js
+++ src/components/App.js
@@ -51,7 +51,7 @@ class App extends React.Component{
                         })}
                     </ul>
                 </div>
-                <Order />
+                <Order burgers = {this.state.burgers} order ={this.state.order} />
                 <MenuAdmin loadSampleBurgers = {this.loadSampleBurgers} addBurger = {this.addBurger}/>
             </div>
         );
--- src/components/Order.js
+++ src/components/Order.js
@@ -1,10 +1,50 @@
 import React from 'react';
 
 class Order extends React.Component{
+
+    renderOrder = (key) =>{
+        const burger = this.props.burgers[key];
+        const count = this.props.order[key];
+        const isAvailable = burger && burger.status === 'available';
+        if(!isAvailable){
+            return <li className='unavailable' key={key}>
+                Извините, {burger ? burger.name : 'бургер'} временно недоступен </li>
+        }
+        return <li key={key}>
+            <span>
+                <span>{count}</span>
+                шт. {burger.name}
+                <span> {count * burger.price}₽</span>
+                <button className='cancellItem'>&times;</button>
+            </span>
+            </li>
+    }
+
     render(){
+
+        const orderIds = Object.keys(this.props.order)
+        const total = orderIds.reduce((prevTotal,key)=>{
+            const burger = this.props.burgers[key];
+            const count = this.props.order[key];
+            const isAvailable = burger && burger.status === 'available';
+            if(isAvailable){
+                return prevTotal + burger.price * count;
+            }
+            return prevTotal;
+            
+        }, 0)
         return(
             <div className='order-wrap'>
                 <h2>Ваш заказ</h2>
+                <ul className='order'>{orderIds.map(this.renderOrder)}
+                </ul>
+                <div className='total'>
+                    <div className='total_wrap'>
+                        <div className='total_wrap-final'>
+                            Итого: {total}₽
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
