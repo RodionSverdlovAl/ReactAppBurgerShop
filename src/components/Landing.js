953436ed1122e7ddafafae03c5f156ae5ed6e652
RodionSverdlovAl <rsverdlovalexandrovich@gmail.com>
2022-01-12 13:47:04 +0300

    add useState and fix class component to functional component

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,59 +1,65 @@
-import React from "react";
+import React, {useState} from "react";
 import restaurants from '../sample-restaurants'
 import PropTypes from 'prop-types';
 
-class Landing extends React.Component {
-    static propTypes = {
-        history: PropTypes.object
+const Landing = props => {
+
+    const [ display, toggleDisplay] = useState(false);
+    const [title, setTitle] = useState('');
+    const [url, setUrl] = useState('');
+
+
+    const displayList = () =>{
+        toggleDisplay(!display);
+    }
+    const getTitle =restaurant=>{
+        const {title, url} = restaurant;
+        setTitle(title);
+        setUrl(url);
+        toggleDisplay(false);
+    }
+    const gotToRestaurant = () =>{
+        props.history.push(`/restaurant/${url}`);
     }
 
-    state = {
-        display: false,
-        title: '',
-        url: '',
-    }
-
-    displayList = () =>{
-        const {display} = this.state;
-        this.setState({display: !display})
-    }
-    getTitle =(restaurants)=>{
-        const {title, url} = restaurants;
-        this.setState({title, url, display:false})
-    }
-    goToRestaurant = () =>{
-        const {url} = this.state;
-        this.props.history.push(`/restaurant/${url}`);
-    }
-
-    render(){
-        return(
-            <>
-                <div className = "restaurant_select">
-                    <div className = "restaurant_select_top">
-                        <div onClick={this.displayList} className = "restaurant_select_top-header font-effect-outline">
-                            {this.state.title ? this.state.title :'Выберите ресторан'}
-                            </div>
-                        <div className = "arrow_picker">
-                            <div className = "arrow_picker-up"></div>
-                            <div className = "arrow_picker-down"></div>
+    return(
+       
+            <div className = "restaurant_select">
+                <div className = "restaurant_select_top">
+                    <div onClick={displayList} className = "restaurant_select_top-header font-effect-outline">
+                        {title ? title :'Выберите ресторан'}
                         </div>
+                    <div className = "arrow_picker">
+                        <div className = "arrow_picker-up"></div>
+                        <div className = "arrow_picker-down"></div>
                     </div>
-
-                    {this.state.display?<div className = "restaurant_select_bottom">
-                        <ul>
-                            {
-                                restaurants.map(restaurants =>{
-                                    return <li onClick={()=>this.getTitle(restaurants)} key ={restaurants.id}>{restaurants.title}</li>
-                                })
-                            }
-                        </ul>
-                    </div>:null}
-                    {this.state.title && !this.state.display ? <button onClick={this.goToRestaurant}>Перейти в ресторан</button>: null}
                 </div>
-            </>
-        )
-    }
+
+                {display ? (
+                  <div className = "restaurant_select_bottom">
+                    <ul>
+                        {restaurants.map(restaurant =>{
+                            return (
+                                <li onClick={()=>getTitle(restaurant)} key ={restaurant.id}>
+                                    {restaurant.title}
+                                </li>
+                            );
+                        })}
+                    </ul>
+                  </div>
+                ):null}
+
+                {title && !display ? (
+                  <button onClick={gotToRestaurant}>Перейти в ресторан</button>
+                ): null}
+            </div>
+        
+    )
+}
+
+Landing.propTypes ={
+    history: PropTypes.object
 }
 
 export default Landing
+
