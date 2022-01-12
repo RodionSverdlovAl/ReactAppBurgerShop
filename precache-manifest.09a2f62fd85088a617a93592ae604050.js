1f4808031e78d1cf1559cf90ed256a80acc9cdb4
RodionSverdlovAl <rsverdlovalexandrovich@gmail.com>
2022-01-12 18:03:44 +0300

    Updates

---
+++ precache-manifest.09a2f62fd85088a617a93592ae604050.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41b5e78be6639c08ca88574003f78c2a",
+    "url": "/ReactAppBurgerShop/index.html"
+  },
+  {
+    "revision": "d825baf07dbb3e818946",
+    "url": "/ReactAppBurgerShop/static/css/main.d3871a48.chunk.css"
+  },
+  {
+    "revision": "2bca10198c7b6c9ac858",
+    "url": "/ReactAppBurgerShop/static/js/2.ba417a70.chunk.js"
+  },
+  {
+    "revision": "d825baf07dbb3e818946",
+    "url": "/ReactAppBurgerShop/static/js/main.5fd63ae0.chunk.js"
+  },
+  {
+    "revision": "06c272242307ea512dd5",
+    "url": "/ReactAppBurgerShop/static/js/runtime-main.7e5f8db0.js"
+  },
+  {
+    "revision": "385ae8f1201379005ad5f888db39e059",
+    "url": "/ReactAppBurgerShop/static/media/burger-logo-4.385ae8f1.jpg"
+  },
+  {
+    "revision": "1d4aace8ece50372c4a71e8334f161a1",
+    "url": "/ReactAppBurgerShop/static/media/down.1d4aace8.svg"
+  },
+  {
+    "revision": "551b5c6b9714b6eafb5739b138485841",
+    "url": "/ReactAppBurgerShop/static/media/up.551b5c6b.svg"
+  }
+]);
